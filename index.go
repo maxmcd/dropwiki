7c7c87952e7823e349eeff931c32f48e5b3237c2
Martino Visintin <vise890@gmail.com>
2015-09-16 17:33:07 +0200

    put exported funcs first

--- index.go
+++ index.go
@@ -7,6 +7,19 @@ import (
 	"sort"
 )
 
+// RenderIndex generates an HTML index from the directory structure
+// of `path`
+func RenderIndex(path string) (string, error) {
+	nodeRepresentation, err := newNodeFrom(path)
+	if err != nil {
+		return "", err
+	}
+
+	renderedIndex := renderIndex(nodeRepresentation)
+
+	return renderedIndex, nil
+}
+
 // nodeSortingWeight determines the sorting order for elements
 // in a []node slice
 type nodeSortingWeight int
@@ -81,6 +94,12 @@ func (f directory) renderIndex(startingDepth int) string {
 	return renderedTitle + renderedChildren
 }
 
+// renderIndex generates an HTML index from the intermediate node
+// representation `start`
+func renderIndex(start node) string {
+	return fmt.Sprintf("<ul>\n%s</ul>", start.renderIndex(1))
+}
+
 // newNodeFrom creates an intermediate representation of the
 // direcory tree starting from `startPath`
 func newNodeFrom(startPath string) (node, error) {
@@ -107,25 +126,6 @@ func newNodeFrom(startPath string) (node, error) {
 	}
 
 	return directory{name: name, contents: contents}, nil
-}
-
-// renderIndex generates an HTML index from the intermediate node
-// representation `start`
-func renderIndex(start node) string {
-	return fmt.Sprintf("<ul>\n%s</ul>", start.renderIndex(1))
-}
-
-// RenderIndex generates an HTML index from the directory structure
-// of `path`
-func RenderIndex(path string) (string, error) {
-	nodeRepresentation, err := newNodeFrom(path)
-	if err != nil {
-		return "", err
-	}
-
-	renderedIndex := renderIndex(nodeRepresentation)
-
-	return renderedIndex, nil
 }
 
 func readDirNames(dirname string) ([]string, error) {
