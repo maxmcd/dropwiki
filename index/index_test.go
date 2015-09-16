578f8f1d8823a5b93d10d0cc387ba84643750292
Martino Visintin <vise890@gmail.com>
2015-09-16 18:29:24 +0200

    adapt tests for new AIT semantics

--- index/index_test.go
+++ index/index_test.go
@@ -14,12 +14,12 @@ func TestRenderIndexRendersIndexFromAPath(t *testing.T) {
 
 	expected := "<ul>\n"
 	expected += "<li><h1>root</h1></li>\n"
-	expected += "<li>root_file.md</li>\n"
+	expected += "<li>root_page.md</li>\n"
 	expected += "<li><h2>level1</h2></li>\n"
-	expected += "<li>level1_file.md</li>\n"
-	expected += "<li>level1_other_file.md</li>\n"
+	expected += "<li>level1_other_page.md</li>\n"
+	expected += "<li>level1_page.md</li>\n"
 	expected += "<li><h3>level2</h3></li>\n"
-	expected += "<li>level2_file.md</li>\n"
+	expected += "<li>level2_page.md</li>\n"
 	expected += "</ul>"
 
 	assert.Equal(t, expected, actual, "should be the same")
@@ -29,16 +29,16 @@ func Test_renderIndex(t *testing.T) {
 	f := section{
 		title: "root",
 		contents: []node{
-			page{title: "root_file.org"},
+			page{title: "root_page.org"},
 			section{
 				title: "level1",
 				contents: []node{
-					page{title: "level1_file.md"},
+					page{title: "level1_page.md"},
 					section{
 						title: "level2",
 						contents: []node{
-							page{title: "level2_file.md"},
-							page{title: "level2_other_file.md"},
+							page{title: "level2_page.md"},
+							page{title: "level2_other_page.md"},
 						},
 					},
 				},
@@ -50,18 +50,18 @@ func Test_renderIndex(t *testing.T) {
 
 	expected := "<ul>\n"
 	expected += "<li><h1>root</h1></li>\n"
-	expected += "<li>root_file.org</li>\n"
+	expected += "<li>root_page.org</li>\n"
 	expected += "<li><h2>level1</h2></li>\n"
-	expected += "<li>level1_file.md</li>\n"
+	expected += "<li>level1_page.md</li>\n"
 	expected += "<li><h3>level2</h3></li>\n"
-	expected += "<li>level2_file.md</li>\n"
-	expected += "<li>level2_other_file.md</li>\n"
+	expected += "<li>level2_page.md</li>\n"
+	expected += "<li>level2_other_page.md</li>\n"
 	expected += "</ul>"
 
 	assert.Equal(t, expected, actual, "should be the same")
 }
 
-func Test_renderIndexRendersPagesBeforeFolders(t *testing.T) {
+func Test_renderIndexRendersPagesBeforeSections(t *testing.T) {
 	f := section{
 		title: "foo",
 		contents: []node{
@@ -94,17 +94,17 @@ func Test_newNodeFromTestDir(t *testing.T) {
 			section{
 				title: "level1",
 				contents: []node{
-					page{"level1_file.md"},
-					page{"level1_other_file.md"},
+					page{"level1_other_page.md"},
+					page{"level1_page.md"},
 					section{
 						title: "level2",
 						contents: []node{
-							page{"level2_file.md"},
+							page{"level2_page.md"},
 						},
 					},
 				},
 			},
-			page{"root_file.md"},
+			page{"root_page.md"},
 		},
 	}
 
